c02108187429bba15a09e1a2f1ad18e7b9bfc492
Andrea <andrea.stella02@universitadipavia.it>
2022-12-13 03:44:44 +0100

    fix

--- denoising_diffusion_pytorch/denoising_diffusion_pytorch.py
+++ denoising_diffusion_pytorch/denoising_diffusion_pytorch.py
@@ -814,7 +814,7 @@ class Trainer(object):
 
         # dataset and dataloader
 
-        self.ds = Dataset(folder, self.image_size, augment_horizontal_flip = augment_horizontal_flip, convert_image_to = convert_image_to, labels = labels)
+        self.ds = Dataset(folder, self.image_size, augment_horizontal_flip = augment_horizontal_flip, convert_image_to = convert_image_to)
         dl = DataLoader(self.ds, batch_size = train_batch_size, shuffle = True, pin_memory = True, num_workers = cpu_count())
 
         dl = self.accelerator.prepare(dl)
@@ -888,7 +888,7 @@ class Trainer(object):
                     data = next(self.dl).to(device)
 
                     with self.accelerator.autocast():
-                        loss = self.model(data)
+                        loss = self.model(data, label)
                         loss = loss / self.gradient_accumulate_every
                         total_loss += loss.item()
 
